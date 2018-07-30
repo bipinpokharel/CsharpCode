3b6b7dd19294903054a02853125a2f06abc5e226
Bipin <bipinpokharel@gmail.com>
2018-07-30 10:38:18 -0500

    test

---
+++ CharacterAt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CharacterAt
+{
+	class Program
+	{
+		/*
+		 * Prompt the user to “Enter a string: “, then read that string.  Next ask the user to “Enter a number: “ and read that number.  
+		 * If the number parses, output the character at that position in the string.  The position is zero-based, meaning that 0 would 
+		 * return the first character of  the string.  If the number is beyond the length of the string, the output should be 
+		 * “Index is out of range”.
+		 *
+		 * Example Input:	supercalifragilistic 12
+		 * Example Output:	g
+		 *
+		 * Hints:	int.TryParse, String.Length, string[]
+		 */
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
